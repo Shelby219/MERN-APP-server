8a92de697768cfa88d28ae8e513c93e32c4f264d
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-12-07 12:56:12 +1000

    added cores whitelist

--- models/user.js
+++ models/user.js
@@ -21,28 +21,6 @@ const User = new Schema({
         type: Schema.Types.ObjectId, 
         ref: "Preferences" 
     },
-    // dietPreferences: {
-    //     vegetarian: {type: Boolean,default: false},
-    //     glutenFree: {type: Boolean,default: false},
-    //     ketogenic: {type: Boolean,default: false},
-    //     vegan: {type: Boolean,default: false},
-    //     pescetarian: {type: Boolean,default: false},
-    //     paleo: {type: Boolean,default: false},
-    // },
-    // healthPreferences: {
-    //     dairy: {type: Boolean,default: false},
-    //     egg: {type: Boolean,default: false},
-    //     gluten: {type: Boolean,default: false},
-    //     grain: {type: Boolean,default: false},
-    //     peanut: {type: Boolean,default: false},
-    //     seafood: {type: Boolean,default: false},
-    //     sesame: {type: Boolean,default: false},
-    //     shellfish: {type: Boolean,default: false},
-    //     soy: {type: Boolean,default: false},
-    //     sulphite: {type: Boolean,default: false},
-    //     treeNut: {type: Boolean,default: false},
-    //     wheat: {type: Boolean,default: false}
-    // },
     fridgeIngredients: {
         type:  Array,
         default: []
