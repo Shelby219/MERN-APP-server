b5e42f329001452c779425699c6efc53512682fd
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2021-01-18 08:04:11 +1000

    App.js update

--- app.js
+++ app.js
@@ -42,6 +42,18 @@ app.use(cors({
 }));
 
 
+//app.options('*', cors())
+//CORS middleware
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'https://fridgemate.netlify.app/');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// }
+
+//  app.use(allowCrossDomain);
+
+
 //
 app.use(cookieParser());
 app.use(bodyParser.json())
