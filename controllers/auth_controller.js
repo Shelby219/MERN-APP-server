8c0c21565d84dca698041e11b201a7ffb7afb91b
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2021-01-18 10:55:03 +1000

    Trust proxt set

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -51,6 +51,7 @@ function loginCreate(req, res) {
     res.status(200);
     res.json({profile: req.user.profile, user: req.user.username, sessionID: req.sessionID, cookie: req.cookies});
     console.log(res)
+
 }
  
 
